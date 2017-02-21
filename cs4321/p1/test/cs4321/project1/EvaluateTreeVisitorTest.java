0b8165e4fdb100250fb545c21521f6708d164452
Jim Z. Li <jim-zed-li@users.noreply.github.com>
2017-02-21 17:11:59 -0500

    Add files via upload

--- cs4321/p1/test/cs4321/project1/EvaluateTreeVisitorTest.java
+++ cs4321/p1/test/cs4321/project1/EvaluateTreeVisitorTest.java
@@ -88,5 +88,49 @@ public class EvaluateTreeVisitorTest {
         n8.accept(v1);
 		assertEquals(3.0, v1.getResult(), DELTA);
 	}
-
+	
+	/*
+	 * Added test to evaluate a second complex 
+	 * expression tree.
+	 */
+	@Test
+	public void testComplexExpressionTree2(){
+		TreeNode n1 = new LeafTreeNode(16.0);
+		TreeNode n2 = new LeafTreeNode(2.0);
+		TreeNode n3 = new DivisionTreeNode(n1, n2);
+		TreeNode n4 = new LeafTreeNode(3.0);
+		TreeNode n5 = new SubtractionTreeNode(n3, n4);
+		TreeNode n6 = new LeafTreeNode(6.0);
+		TreeNode n7 = new AdditionTreeNode(n5, n6);
+		EvaluateTreeVisitor v1 = new EvaluateTreeVisitor();
+		n7.accept(v1);
+		assertEquals(11.0, v1.getResult(), DELTA);
+		TreeNode n8 = new AdditionTreeNode(n6, n5);
+		EvaluateTreeVisitor v2 = new EvaluateTreeVisitor();
+		n8.accept(v2);
+		assertEquals(11.0, v2.getResult(), DELTA);
+	}
+	
+	/*
+	 * Added test to evaluate a third complex expression
+	 * tree.
+	 */
+	@Test
+	public void testComplexExpressionTree3(){
+		TreeNode n1 = new LeafTreeNode(12.0); 
+		TreeNode n2 = new LeafTreeNode(2.0); 
+		TreeNode n3 = new LeafTreeNode(3.0); 
+		TreeNode n4 = new UnaryMinusTreeNode(n1); 
+		TreeNode n5 = new MultiplicationTreeNode(n2, n3);
+        TreeNode n6 = new DivisionTreeNode(n4, n5);
+        TreeNode n7 = new LeafTreeNode(5.0);
+        TreeNode n8 = new DivisionTreeNode(n6, n7);
+        EvaluateTreeVisitor v1 = new EvaluateTreeVisitor();
+        n8.accept(v1);
+        assertEquals(-0.4, v1.getResult(), DELTA);
+        TreeNode n9 = new DivisionTreeNode(n7, n6);
+        EvaluateTreeVisitor v2 = new EvaluateTreeVisitor();
+        n9.accept(v2);
+        assertEquals(-2.5, v2.getResult(), DELTA);
+	}
 }
